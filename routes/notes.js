f9436c1bd704af9e246b2c993f6f3551ce840ddf
Dallas Sybrowsky <dallassybrowsky@Dallass-MacBook-Pro.local>
2022-12-12 21:28:15 -0800

    Upload of files without Develop folder and with working delete route

--- routes/notes.js
+++ routes/notes.js
@@ -1,14 +1,15 @@
 const notes = require("express").Router();
 const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
 const uuid = require("../helpers/uuid");
+const fs = require("fs");
 
-notes.get("/", (req, res) => { // GET Route for retrieving all the notes
+notes.get("/", (req, res) => {
+  // GET Route for retrieving all the notes
   console.log(`${req.method} request received for notes`);
-  readFromFile("./db/db.json").then((data) =>
-    res.json(JSON.parse(data))
-  );
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
-notes.post("/", (req, res) => { // POST Route for a new note
+notes.post("/", (req, res) => {
+  // POST Route for a new note
   console.log(req.body);
 
   const { title, text } = req.body;
@@ -33,14 +34,16 @@ notes.post("/", (req, res) => { // POST Route for a new note
 });
 
 // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete.
-notes.delete('/:id', (req, res) => {
-  // reading notes form db.json
-  let db = JSON.parse(fs.readFileSync("./db/db.json"));
-  // removing note with id
-  let deleteNotes = db.filter((item) => item.id !== req.params.id);
-  // Rewriting note to db.json
-  fs.writeFileSync("./db/db.json", JSON.stringify(deleteNotes));
-  res.json(deleteNotes);
+notes.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  readFromFile("./db/db.json").then((data) => {
+    const parsedNotes = JSON.parse(data);
+    const deleteNote = parsedNotes.filter((note) => note.id !== id);
+    fs.writeFile("./db/db.json", JSON.stringify(deleteNote), (err) =>
+      err ? console.err(err) : console.info(`\nNote ${id} has been deleted\n`)
+    );
+    res.json(deleteNote);
+  });
 });
 
 module.exports = notes;
